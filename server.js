fbb4be21f88aad5787e4f92c4ecd6a78f65e40a0
Blake <blakekelley127@gmail.com>
2018-12-01 17:56:43 -0700

    Updating listening port to use the process.env.PORT environment variable or port 3000

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ app.post('/', function (req, res) {
 	});
 })
 
-app.listen(3000, function () {
-	console.log('Weather app listening on port 3000!')
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function () {
+	console.log('Weather app listening on port ', app.get('port'))
 })
